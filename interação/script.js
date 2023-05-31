b2ccdddceb0fd8bf05b5d3829b3c367700e3670d
Sabrina rodrigues <110910201+Sabrinarodri18@users.noreply.github.com>
2023-05-30 23:46:30 -0300

    interações do DOM

---
+++ interação/script.js
@@ -0,0 +1,76 @@
+//array com as palavras disponiveis 
+
+//alteração 
+var availableWords = ['amor', 'mar', 'lealdade', 'contente', 'luz', 'flores', 'emoção'];
+
+
+function checkPoem(){
+    var input = document.getElementById('textarea').value.toLowerCase() 
+    var words = input.split(' ');
+
+    var isValid = words.some(function (word){
+        return availableWords.includes(word);
+    });
+
+
+    var result = document.getElementById('result');
+
+    if (isValid) {
+        result.textContent = 'Você é realmente uma pessoa criativa, parabéns!!';
+    } else {
+        result.textContent = 'Você  não usou uma das palavras combinadas, faça novamente.'
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {  //carregar o event listener
+ let btncriar =  document.getElementById("botao"); //criamos variaveis para pegar elementos botão e o section
+ let div =  document.getElementById("section");
+
+ btncriar.addEventListener('click', function() {
+
+
+ 	let painel = document.createElement('div');
+ 	div.appendChild(painel);
+
+ 	let textarea = document.createElement('textarea');
+ 	textarea.setAttribute('id', 'textarea');
+ 	painel.appendChild(textarea);
+
+ 	let btnExcluir = document.createElement('button');
+ 	btnExcluir.setAttribute('id', 'btn-remove');
+ 	btnExcluir.textContent = "exluir";
+ 	painel.appendChild(btnExcluir);
+ 	btncriar.style.display = 'none';
+
+ 	btnExcluir.addEventListener('click', function() {
+ 		painel.remove();
+ 		btncriar.style.display = 'flex';
+	})
+});
+
+
+
+
+
+})
+
+     
+     
+     
+     /*setatribut modificar ou add atributo
+     append elemento filho dentro dele pq ele é o pai */
+
+     const poemas = document.querySelectorAll('.poema');
+     poemas.forEach(poema => {
+       const stars = poema.querySelectorAll('.rating input[type="radio"]');
+       const classificacao = poema.querySelector('p');
+     
+       stars.forEach(star => {
+         star.addEventListener("change", function() {
+           const value = this.value;
+           classificacao.textContent = value + "/3";
+         });
+       });
+     });
